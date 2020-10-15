e182d2ccb4e6a9d7bd7a84e136dcc7a9359673f3
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-15 11:50:30 +0900

    Add files via upload

---
+++ spring-jpa3/src/main/java/com/example/demo/SpringJpa21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpa21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpa21Application.class, args);
+	}
+
+}
