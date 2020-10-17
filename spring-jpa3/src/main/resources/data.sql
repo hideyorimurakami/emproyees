b2e73050c91117ee22e29a071a40293f866eba09
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-17 17:18:41 +0900

    Update data.sql

--- spring-jpa3/src/main/resources/data.sql
+++ spring-jpa3/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.department (id,name) VALUES (1,'営業');
-INSERT INTO public.department (id,name) VALUES (2,'経理');
-INSERT INTO public.department (id,name) VALUES (3,'開発');
-INSERT INTO public.department (id,name) VALUES (4,'総務');
+INSERT INTO department (id,name) VALUES (1,'営業');
+INSERT INTO department (id,name) VALUES (2,'経理');
+INSERT INTO department (id,name) VALUES (3,'開発');
+INSERT INTO department (id,name) VALUES (4,'総務');
